2532feb1d32aab5a2dcce84a470b80984f87c3c6
ninaraynor <ninaraynor@gmail.com>
2023-11-29 23:23:40 -0500

    expanding on js plan; contenteditable to allow letters typed into gameboard

--- js/main.js
+++ js/main.js
@@ -15,14 +15,61 @@ const gameBoard = {
       ['', '', '', '', '', '', '', '', ''],
     ]};
 
+const answers = []; 
+
+
 /*----- state variables -----*/
+
+let selectedCell;
+let wordAcross;
+let wordDown;
+let isSuccess;
 
 
 /*----- cached elements -----*/
+
+// directionAcross & directionDown (from HTML ID)
 
 
 /*----- event listeners -----*/
 
 
+
 /*----- functions -----*/
 
+init();
+
+// define init function here...then call render()
+
+function init() {
+    grid = [
+      ['', '', '', '', '', '', '', '', ''], // a1
+      ['', '', '', '', '', '', '', '', ''], // a2
+      ['', '', '', '', '', '', '', '', ''], // a3
+      ['', '', '', '', '', '', '', '', ''], // a4
+      ['', '', '', '', '', '', '', '', ''], // a5
+      ['', '', '', '', '', '', '', '', ''], // a6
+      ['', '', '', '', '', '', '', '', ''], // a7
+      ['', '', '', '', '', '', '', '', ''], // a8
+      ['', '', '', '', '', '', '', '', ''], // a9
+    // d1, d2, d3, d4, d5, d6, d7, d8, d9
+    ];
+
+    // call render function here
+    ;
+}
+
+// render: 
+
+// create loop for populating grid for gameboard
+
+// function for checking word fits
+function checkWord() {
+
+  // for loop to check each row 
+  
+  // for loop to check each column 
+}
+
+
+// ternary to find if word successfully fits using isSucess
